b7ccae20ac14b64375e4dda1689d9235718f0663
Daniel Valkanov <122799783+Daniel8Valkanov8@users.noreply.github.com>
2024-03-06 18:52:53 +0200

    Add files via upload

---
+++ SpringDataAdvancedQuering/src/main/java/com/example/springintro/CommandLineRunnerImpl.java
@@ -0,0 +1,86 @@
+package com.example.springintro;
+
+import com.example.springintro.model.entity.AgeRestriction;
+import com.example.springintro.model.entity.Book;
+import com.example.springintro.service.AuthorService;
+import com.example.springintro.service.BookService;
+import com.example.springintro.service.CategoryService;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Scanner;
+
+@Component
+public class CommandLineRunnerImpl implements CommandLineRunner {
+
+    private final CategoryService categoryService;
+    private final AuthorService authorService;
+    private final BookService bookService;
+
+    public CommandLineRunnerImpl(CategoryService categoryService, AuthorService authorService, BookService bookService) {
+        this.categoryService = categoryService;
+        this.authorService = authorService;
+        this.bookService = bookService;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        seedData();
+
+        //printAllBooksAfterYear(2000);
+//        printAllAuthorsNamesWithBooksWithReleaseDateBeforeYear(1990);
+     //   printAllAuthorsAndNumberOfTheirBooks();
+       // pritnALlBooksByAuthorNameOrderByReleaseDate("George", "Powell");
+        //printBookByAgeRestriction();
+        printBookBetweenPrice();
+    }
+
+    private void printBookBetweenPrice() {
+        List<String> books = bookService.findBookBetweenPrice();
+        books.forEach(b->System.out.println(b));
+    }
+
+    private void printBookByAgeRestriction() {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        AgeRestriction ageRestriction = AgeRestriction.valueOf(input.toUpperCase());
+            List<String> books = bookService.findByAgeRestriction(ageRestriction);
+            books.forEach(b->System.out.println(b));
+    }
+
+
+
+    private void pritnALlBooksByAuthorNameOrderByReleaseDate(String firstName, String lastName) {
+        bookService
+                .findAllBooksByAuthorFirstAndLastNameOrderByReleaseDate(firstName, lastName)
+                .forEach(System.out::println);
+    }
+
+    private void printAllAuthorsAndNumberOfTheirBooks() {
+        authorService
+                .getAllAuthorsOrderByCountOfTheirBooks()
+                .forEach(System.out::println);
+    }
+
+    private void printAllAuthorsNamesWithBooksWithReleaseDateBeforeYear(int year) {
+        bookService
+                .findAllAuthorsWithBooksWithReleaseDateBeforeYear(year)
+                .forEach(System.out::println);
+    }
+
+    private void printAllBooksAfterYear(int year) {
+        bookService
+                .findAllBooksAfterYear(year)
+                .stream()
+                .map(Book::getTitle)
+                .forEach(System.out::println);
+    }
+
+    private void seedData() throws IOException {
+        categoryService.seedCategories();
+        authorService.seedAuthors();
+        bookService.seedBooks();
+    }
+}
---
+++ SpringDataAdvancedQuering/src/main/java/com/example/springintro/repository/BookRepository.java
@@ -0,0 +1,24 @@
+package com.example.springintro.repository;
+
+import com.example.springintro.model.entity.AgeRestriction;
+import com.example.springintro.model.entity.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long> {
+
+    List<Book> findAllByReleaseDateAfter(LocalDate releaseDateAfter);
+
+    List<Book> findAllByReleaseDateBefore(LocalDate releaseDateBefore);
+
+    List<Book> findAllByAuthor_FirstNameAndAuthor_LastNameOrderByReleaseDateDescTitle(String author_firstName, String author_lastName);
+    List<Book> findAllByAgeRestriction(AgeRestriction ageRestriction);
+
+    @Query("SELECT b FROM Book b WHERE b.price > 5.0 and b.price< 40.0")
+    List<Book> findBookBetweenPrice();
+}
